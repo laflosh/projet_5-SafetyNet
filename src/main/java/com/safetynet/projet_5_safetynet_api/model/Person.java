ea8c5d2302d62abd04d3f95bca13f87a6af7f58d
laflosh <l_florent_l@hotmail.fr>
2024-09-09 18:01:18 +0200

    Création model Person

---
+++ src/main/java/com/safetynet/projet_5_safetynet_api/model/Person.java
@@ -0,0 +1,102 @@
+package com.safetynet.projet_5_safetynet_api.model;
+
+public class Person {
+
+	private String firstName;
+	
+	private String lastName;
+	
+	private String adress;
+	
+	private String city;
+	
+	private int zip;
+	
+	private String phone;
+	
+	private String email;
+	
+	public String getFirstName() {
+		
+		return firstName;
+		
+	}
+	
+	public void setFirstName(String firstName) {
+		
+		this.firstName = firstName;		
+	}
+	
+	public String getLastName() {
+		
+		return lastName;
+		
+	}
+	
+	public void setLastName(String lastName) {
+		
+		this.lastName = lastName;
+		
+	}
+	
+	public String getAdress() {
+		
+		return adress;
+		
+	}
+	
+	public void setAdress(String adress) {
+		
+		this.adress = adress;
+		
+	}
+	
+	public String getCity() {
+		
+		return city;
+		
+	}
+	
+	public void setCity(String city) {
+		
+		this.city = city;
+		
+	}
+	
+	public int getZip() {
+		
+		return zip;
+		
+	}
+	
+	public void setZip(int zip) {
+		
+		this.zip = zip;
+		
+	}
+	
+	public String getPhoneNumber() {
+		
+		return phone;
+		
+	}
+	
+	public void setPhoneNumber(String phone) {
+		
+		this.phone = phone;
+		
+	}
+	
+	public String getEmail() {
+		
+		return email;
+		
+	}
+	
+	public void setEmail(String email) {
+		
+		this.email = email;
+		
+	}
+	
+}

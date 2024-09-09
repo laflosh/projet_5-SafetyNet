68f4fd2285e5b8715b1b0be64b923e954216e642
laflosh <l_florent_l@hotmail.fr>
2024-09-09 17:06:32 +0200

    Test log lancement app + import data json

--- src/main/java/com/safetynet/projet_5_safetynet_api/Projet5SafetynetApiApplication.java
+++ src/main/java/com/safetynet/projet_5_safetynet_api/Projet5SafetynetApiApplication.java
@@ -1,13 +1,20 @@
 package com.safetynet.projet_5_safetynet_api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Projet5SafetynetApiApplication {
 
+	private static final Logger logger = LogManager.getLogger("Projet5SafetynetApiApplication");
+	
 	public static void main(String[] args) {
+		
+		logger.info("Initializing API SafetyNet.");
 		SpringApplication.run(Projet5SafetynetApiApplication.class, args);
+		
 	}
 
 }

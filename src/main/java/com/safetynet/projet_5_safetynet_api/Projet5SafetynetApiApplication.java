8a8b2123a5914508026d8bd4255ad134cb7e9ffb
laflosh <l_florent_l@hotmail.fr>
2024-09-09 16:40:37 +0200

    Initialisation projet

---
+++ src/main/java/com/safetynet/projet_5_safetynet_api/Projet5SafetynetApiApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.projet_5_safetynet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet5SafetynetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet5SafetynetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/projet_5_safetynet_api/Projet5SafetynetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.projet_5_safetynet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet5SafetynetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

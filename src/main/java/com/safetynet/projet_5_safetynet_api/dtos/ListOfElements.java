ef023154ff8fa7d2f08164ad05579a68f646f218
laflosh <l_florent_l@hotmail.fr>
2024-09-11 14:20:33 +0200

    implementation model firestations and medicalrecords in dto

--- src/main/java/com/safetynet/projet_5_safetynet_api/dtos/ListOfElements.java
+++ src/main/java/com/safetynet/projet_5_safetynet_api/dtos/ListOfElements.java
@@ -5,6 +5,8 @@ package com.safetynet.projet_5_safetynet_api.dtos;
 
 import java.util.List;
 
+import com.safetynet.projet_5_safetynet_api.model.FireStations;
+import com.safetynet.projet_5_safetynet_api.model.MedicalRecords;
 import com.safetynet.projet_5_safetynet_api.model.Person;
 
 /**
@@ -14,9 +16,9 @@ public class ListOfElements {
 
 	protected List<Person> persons;
 	
-	//protected List<FireStations> fireStations;
+	protected List<FireStations> fireStations;
 	
-	//protected List<MedicalRecords> medicalRecords;
+	protected List<MedicalRecords> medicalRecords;
 	
 	public List<Person> getPersons() {
 		
@@ -27,6 +29,30 @@ public class ListOfElements {
 	public void setPersons(List<Person> persons) {
 		
 		this.persons = persons;
+		
+	}
+	
+	public List<FireStations> getFireStations() {
+		
+		return fireStations;
+		
+	}
+	
+	public void setFireStations(List<FireStations> fireStations) {
+		
+		this.fireStations = fireStations;
+		
+	}
+	
+	public List<MedicalRecords> getMedicalRecords() {
+		
+		return medicalRecords;
+		
+	}
+	
+	public void setMedicalRecords(List<MedicalRecords> medicalRecords) {
+		
+		this.medicalRecords = medicalRecords;
 		
 	}
 	
